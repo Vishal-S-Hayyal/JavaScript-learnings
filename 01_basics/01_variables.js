0a72d867b48b715051ac94a62d634c39e206a3c1
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2023-10-31 16:40:40 +0530

    Learning variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 14453
+let accountEmail = "vishal2003@gmail.com"
+var accountPassword = "1234"
+accountCity = "Hunsgi"
+let accountState;
+
+// accountId = 12 // not allowed
+
+accountEmail = "vh@vh.com"
+accountPassword = "212121"
+accountity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+Because oof issue in block scope and functional scope 
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
