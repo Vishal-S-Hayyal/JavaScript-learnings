acdc12cebbd1d8ce9586e37e6e4a00faf6ec062a
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2023-11-01 14:13:31 +0530

    1/11/2023

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,14 +1,14 @@
-const accountId = 14453
-let accountEmail = "vishal2003@gmail.com"
-var accountPassword = "1234"
-accountCity = "Hunsgi"
+const accountId = 14453;
+let accountEmail = "vishal2003@gmail.com";
+var accountPassword = "1234";
+accountCity = "Hunsgi";
 let accountState;
 
 // accountId = 12 // not allowed
 
-accountEmail = "vh@vh.com"
-accountPassword = "212121"
-accountity = "Bengaluru"
+accountEmail = "vh@vh.com";
+accountPassword = "212121";
+accountity = "Bengaluru";
 
 console.log(accountId);
 
@@ -17,5 +17,10 @@ Prefer not to use var
 Because oof issue in block scope and functional scope 
 */
 
-
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
