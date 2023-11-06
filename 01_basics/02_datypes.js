cbe0da05e57a67c8f1d880e9416c8689b418c55a
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2023-11-06 23:20:37 +0530

    datatypes

---
+++ 01_basics/02_datypes.js
@@ -0,0 +1,23 @@
+"use strict";  //treat all code as newer version
+
+//alert (3 + 3) // we are using node js, not browser.
+
+console.log(3 + 
+    3) // code redability should be high
+
+console.log("Vishal");    
+
+let name = "Vishal"  //string 
+let age = 21   //age 
+let isLoggedIn = false  //boolean
+
+/* few (primitive) datadypes available in JS
+number => 2 to power 53
+bigint 
+string => ""
+boolean => true/false
+null => standalone value  (typeof(nul) gives object as output) *interview question
+undefined =>   (typeof(undefined) = undefined)
+symbol => unique */
+
+console.log(typeof null);
