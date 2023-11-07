c5fa1e3a07b63a3ab0953b80e051d0ca6bb53f78
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2023-11-07 23:16:58 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+
+// javaScript conversion outputs for few examples
+// "33" => 33
+// "33abc" => NaN (Not a number)
+// true = 1 // false = 0
+// Null = 0
+// undefined = NaN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// " " => false
+// "Vishal" => true
+
+let number = 33
+
+let stringNumber = String(number)
+console.log(typeof stringNumber);
+
