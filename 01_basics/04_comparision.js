f328f59524efb1bac0919278f168b492bbad000f
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2023-11-12 03:38:43 +0530

    Learnt Comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// Simple Comparisions 
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); //automatically Js converts datatype 
+// console.log("02" > 1);
+
+console.log(null > 0); //Output false
+console.log(null == 0); //Output false
+console.log(null >= 0); //Ouput true
+// in Js (==) and comparision operators work differently if we use comparision operators it assumes null to be 0 
+
+// === Strict check operator This even compares datatype and gives output
+
+console.log("2" === 2); //Output false
