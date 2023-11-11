be251deb007dd0ba91d4f533cc47768bf8682842
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2023-11-12 03:20:40 +0530

    Operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,5 +26,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let number = 33
 
 let stringNumber = String(number)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
 
+
+// *******************************OPERATIONS*******************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-3);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Vishal"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);   //Whatever you write first in the console log it will be assumed to operate
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((2 * 3) +( 4 % 2) - 1); // using paranthesis is the best practice 
+
+// console.log(+true); //Output will be given as 1 beacuse it assumes you are doing arithmetic operation
+// console.log(+""); // ""(empty) paranthesis gives 0 or false output
+
+let gameCounter1 = 100
+let gameCounter2 = 100
+const prefix = ++gameCounter1
+const postfix = gameCounter2++
+
+console.log(prefix,gameCounter1); //Output (101,101)
+console.log(postfix,gameCounter2); //Output (100,101)
