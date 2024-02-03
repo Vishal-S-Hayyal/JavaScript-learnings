6718ffbac798ffd972c8646256307fcc41cfe621
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2024-02-03 23:56:55 +0530

    basics of object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -18,10 +18,21 @@ const jsUser = {
 
 // console.log(jsUser.age);
 // console.log(jsUser["age"]);
-// console.log(typeof jsUser[mySym]);
+// console.log(typeof jsUser[mySym]); //sometimes it is better to access attributes of objects using square bracket INTERVIEW POV 
 
 jsUser.location = 'Banglore'
-Object.freeze(jsUser)
+// Object.freeze(jsUser)
 // console.log(jsUser);
 jsUser.location = 'Chennai'
-console.log(jsUser);
+// console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("Hello jsUser");
+}
+// console.log(jsUser.greeting);
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello jsUser, ${this.name}`);
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
