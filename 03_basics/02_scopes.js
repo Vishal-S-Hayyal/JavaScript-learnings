3ff328e336d75f67f69dcd8ec4a3e2edc5e20541
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2024-02-07 23:38:48 +0530

    7-2-2024

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,32 @@
+
+
+if(true){
+    const a = 10
+    let b = 20
+    var c = 30 //var datatype defy's the logic of scopes 
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// variables declared inside any loops,objects,functions will be bound to that specific scope,so they are called blockscope
+// variables declared outside indipendently are called globalscope variables
+
+function one(){
+    const username = "Vishal"
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()
+}
+// one()
+
+// Child function can access attributes of parent function but vice versa is not true
+
+function addone(num){
+    return num+1
+}
+addone(4)
