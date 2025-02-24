216448fcf952ebb4e046c841f21742a71673a73e
Vinuta Hayyal <vishalhayyal12@gmail.com>
2025-02-25 02:28:46 +0530

    Preparation Over

--- Dom_manipulation/script.js
+++ Dom_manipulation/script.js
@@ -33,23 +33,64 @@
 
 //Event Handling
 
-let modebtn = document.querySelector("#mode");
-let body = document.querySelector("body");
-let currmode = "light";
+// let modebtn = document.querySelector("#mode");
+// let body = document.querySelector("body");
+// let currmode = "light";
 
-modebtn.addEventListener("click", () => {
-    if(currmode==="light"){
-        currmode="dark";
-        body.classList.add("dark");
-        body.classList.remove("light");
-    }
-    else{
-        currmode="light";
-        body.classList.add("light");
-        body.classList.remove("dark");
-    }
+// modebtn.addEventListener("click", () => {
+//     if(currmode==="light"){
+//         currmode="dark";
+//         body.classList.add("dark");
+//         body.classList.remove("light");
+//     }
+//     else{
+//         currmode="light";
+//         body.classList.add("light");
+//         body.classList.remove("dark");
+//     }
 
-    console.log(currmode);
+//     console.log(currmode);
     
     
-});
+// });
+
+// //Objects and Classes
+
+// let DATA = "Sample Data"
+// class user {
+//     constructor(name,email) {
+//         this.name = name;
+//         this.email = email;
+//     }
+//     viewData(){
+//         console.log("Data = ", DATA);   
+//     }
+// }
+
+// let student1 = new user("Vishal", "vishal@gmail.com")
+// let student2 = new user("Axay", "Axay@gmail.com");
+
+// class Admin extends user{
+//     constructor(name, email){
+//         super(name, email);
+//     }
+//     editData(){
+//         DATA = "new Data";
+//     }
+
+// }
+
+// let admin1 = new Admin("Vishal","Vishal@gmail.com");
+
+const url = "https://www.reddit.com/r/news/.json";
+
+const getdata = async () =>{
+    let response = await fetch(url);
+    console.log(response);
+    let news = await response.json();
+    let randomIndex = Math.floor(Math.random() * news.data.children.length);
+    let singleNews = news.data.children[randomIndex].data.title;
+    
+    console.log(singleNews);
+    
+}
